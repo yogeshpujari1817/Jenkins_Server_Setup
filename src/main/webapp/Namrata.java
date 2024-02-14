2084f95b0253e1ab7cf6022e866ef5501694c349
Container_Coder <yogeshpujari1817@gmail.com>
2024-02-14 20:57:36 +0530

    Namrata.java file is added

---
+++ src/main/webapp/Namrata.java
@@ -0,0 +1,9 @@
+public class ValentineWishes {
+    public static void main(String[] args) {
+        String recipientName = "Namrata";
+        String message = "Happy Valentine's Day, " + recipientName + "! 💕✨🌹"; 
+
+        System.out.println(message);
+    }
+}
+``
